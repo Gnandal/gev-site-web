1b8dcf00a971fbfca525bb05770599419f498fe3
Gnandal <egnandal500@gmail.com>
2022-06-12 12:15:54 +0100

    init

---
+++ src/main/java/td/gev/site/controller/GevController.java
@@ -0,0 +1,13 @@
+package td.gev.site.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GevController {
+    @GetMapping
+    public String homePage(Model model){
+        return "index";
+    }
+}
